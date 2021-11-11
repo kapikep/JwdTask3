cc9e3603ab1e4e9f552efc783e21695ce5d69c47
Kep <kapikep@gmail.com>
2021-11-12 02:16:05 +0300

    Commit

---
+++ src/main/java/by/epam/jwd3/Calculator.java
@@ -0,0 +1,4 @@
+package by.epam.jwd3;
+
+public interface Calculator {
+}
---
+++ src/main/java/by/epam/jwd3/CalculatorFactory.java
@@ -0,0 +1,11 @@
+package by.epam.jwd3;
+
+public class CalculatorFactory {
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: by.htp.jaxbex
+     *
+     */
+    public CalculatorFactory() {
+    }
+
+}
